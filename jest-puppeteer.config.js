32abe969be83371a8a4e71995211169abc8e8f18
Ilya195 <vodoley_agent@inbox.ru>
2023-03-26 10:30:33 +0600

    Initial commit

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    launch: {
+        slowMo: 700,
+        headless: false,
+        defaultViewport: null,
+        args: ['--start-maximized'] //— используем максимальный размер окна браузера
+      },
+    
+  };
+  
---
+++ net.test.js
@@ -0,0 +1,52 @@
+
+const { clickElement, getText } = require("./lib/commands.js");
+const daysWeek = require("./lib/util.js");
+
+
+let page;
+
+beforeEach(async () => {
+    page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
+});
+
+afterEach(() => {
+    page.close();
+});
+
+describe("Booking tickets", () => {
+    beforeEach(async () => {
+        await page.goto("http://qamid.tmweb.ru/client/index.php", {
+            timeout: 60000,
+        });
+        await clickElement(page, daysWeek.thirdDay);
+    });
+
+
+    test("Should book one seat", async () => {
+        await clickElement(page, "[data-seance-id='139']");
+        await clickElement(page, 1, 1);
+        await clickElement(page, ".acceptin-button");
+
+        const actual = await getText(page, "h2.ticket__check-title");
+        expect(actual).contain("Вы выбрали билеты:");
+    }, 60000);
+
+
+    test("Should book one VIP seat", async () => {
+        await clickElement(page, "[data-seance-start='720']");
+        await clickElement(page, 2, 2);
+        await clickElement(page, ".acceptin-button");
+
+        const actual = await getText(page, "h2.ticket__check-title");
+        expect(actual).contain("Вы выбрали билеты:");
+    }, 60000);
+
+
+    test("Should don't booking seat", async () => {
+        await clickElement(page, "[data-seance-id='129']");
+
+        const isDisabled = await page.$eval("button", (button) => button.disabled);
+        expect(isDisabled).to.equal(true);
+    }, 60000);
+});
