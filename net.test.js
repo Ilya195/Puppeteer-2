c9091c5893e5bfe0146569338b283d7cd4f6a038
Ilya195 <vodoley_agent@inbox.ru>
2023-04-06 09:21:45 +0600

    Second commit

--- net.test.js
+++ net.test.js
@@ -14,32 +14,33 @@ afterEach(() => {
     page.close();
 });
 
+
 describe("Booking tickets", () => {
     beforeEach(async () => {
         await page.goto("http://qamid.tmweb.ru/client/index.php", {
             timeout: 60000,
         });
-        await clickElement(page, daysWeek.thirdDay);
+        console.log(await page.title());
     });
 
 
     test("Should book one seat", async () => {
         await clickElement(page, "[data-seance-id='139']");
-        await clickElement(page, 1, 1);
+        await clickElement(page, ".buying-scheme__chair_standart:not(.buying-scheme__chair_taken)");
         await clickElement(page, ".acceptin-button");
 
         const actual = await getText(page, "h2.ticket__check-title");
-        expect(actual).contain("Вы выбрали билеты:");
+        expect(actual).toEqual("Вы выбрали билеты:");
     }, 60000);
 
 
     test("Should book one VIP seat", async () => {
-        await clickElement(page, "[data-seance-start='720']");
-        await clickElement(page, 2, 2);
+        await clickElement(page, "[data-seance-id='140']");
+        await clickElement(page, ".buying-scheme__chair.buying-scheme__chair_vip");
         await clickElement(page, ".acceptin-button");
 
         const actual = await getText(page, "h2.ticket__check-title");
-        expect(actual).contain("Вы выбрали билеты:");
+        expect(actual).toEqual("Вы выбрали билеты:");
     }, 60000);
 
 
@@ -47,6 +48,6 @@ describe("Booking tickets", () => {
         await clickElement(page, "[data-seance-id='129']");
 
         const isDisabled = await page.$eval("button", (button) => button.disabled);
-        expect(isDisabled).to.equal(true);
+        expect(isDisabled).toEqual(true);
     }, 60000);
 });
